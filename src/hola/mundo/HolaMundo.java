6a4eb448ec0df34ee77f05a2b18c6d45e6b40069
nelson <xav.ice@gmail.com>
2011-05-22 20:00:54 -0500

    Primer Commit

---
+++ src/hola/mundo/HolaMundo.java
@@ -0,0 +1,17 @@
+package hola.mundo;
+
+public class HolaMundo {
+
+	public HolaMundo() {
+		System.out.println("Hola Mundo xD");
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
